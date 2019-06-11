12ebdc429e1d68e42634085478b89cbfbb915b05
aaugmentum <azamat9812@gmail.com>
2019-06-11 20:20:13 +0500

    Add explicit type declaration for argument map

--- lib/services/tdlib/platform-linker.dart
+++ lib/services/tdlib/platform-linker.dart
@@ -4,10 +4,10 @@ const _platform = const MethodChannel('tdjsonlib');
 
 Future<int> create() async => await _platform.invokeMethod('create');
 
-Future<void> send({int client, String request}) async => await _platform.invokeMethod('send', {'client': client, 'request': request});
+Future<void> send({int client, String request}) async => await _platform.invokeMethod('send', <String, dynamic>{'client': client, 'request': request});
 
-Future<String> receive({int client, int delay}) async => await _platform.invokeMethod('receive',  {'client': client, 'delay': delay});
+Future<String> receive({int client, int delay}) async => await _platform.invokeMethod('receive',  <String, dynamic>{'client': client, 'delay': delay});
 
-Future<String> execute({int client, String request}) async => await _platform.invokeMethod('execute', {'client': client, 'request': request});
+Future<String> execute({int client, String request}) async => await _platform.invokeMethod('execute', <String, dynamic>{'client': client, 'request': request});
 
 Future<void> destroy({int client}) async => await _platform.invokeMethod('destroy',  <String, dynamic>{'client': client});
