aff8c20eab15a0bbacb752bdddad1ff0463f30a3
Azamat Vakhobov <azamat9812@gmail.com>
2019-07-13 12:45:05 +0500

    Add music channel

--- android/app/src/main/java/com/example/audiogram/MainActivity.java
+++ android/app/src/main/java/com/example/audiogram/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.audiogram;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  private static final String CHANNEL = "tdjsonlib";
+  private static final String CHANNEL_TD = "tdjsonlib";
+  private static final String CHANNEL_MUSIC = "music";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
 
-    MethodChannel tdlibMc = new MethodChannel(getFlutterView(), CHANNEL);
+    MethodChannel tdlibMc = new MethodChannel(getFlutterView(), CHANNEL_TD);
     tdlibMc.setMethodCallHandler(new TdlibChannelThread(this));
+
+    MethodChannel musicMc = new MethodChannel(getFlutterView(), CHANNEL_MUSIC);
+    musicMc.setMethodCallHandler((methodCall, result) -> {
+      
+    });
   }
 }
